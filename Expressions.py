012c65bf8da9448218f331b5b09e21b2fc9a3c11
Yasin Şensoy <73845925+sensoyyasin@users.noreply.github.com>
2021-06-25 18:52:20 +0300

    Add files via upload

---
+++ Expressions.py
@@ -0,0 +1,24 @@
+#Comments in python
+#Some documents
+#String Conversions
+
+sval = "123"
+type(sval)
+#<class "str">
+
+ival = int(sval)
+type(ival)
+#<class "int">
+
+print(ival+1)
+#124
+
+#Integer Division
+print(10/2)
+#5.0
+print(9/2)
+#4.5
+print(99/100)
+#0.99
+print(10.0/2.0)
+#5.0
